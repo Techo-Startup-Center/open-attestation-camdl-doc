b0ccb4823754a2373cb228003d5ca4c87cd56412
sereyvathanatum <sereyvathanatum@gmail.com>
2023-12-08 10:27:29 +0700

    fix dns issue

--- oa-svc/utils/dns.js
+++ oa-svc/utils/dns.js
@@ -12,55 +12,51 @@ const getDnsTxt = async (url) => {
       }
     );
 
-    if (!data.Answer) {
-      return [];
-    }
-
-    for (const txt of data.Answer) {
-      if (url !== txt.name) {
-        const msg = "Mistached DNS name";
-        const err = new Error();
-        err.status = 400;
-        err.code = "BAD_REQUEST";
-        err.message = msg;
-        throw err;
-      }
-
-      let dataList = txt.data.replace(/['"]+/g, "").split(" ");
-      if (dataList[0] !== "openatts") {
-        const msg = "Unsupported attestation framework";
-        const err = new Error();
-        err.status = 400;
-        err.code = "BAD_REQUEST";
-        err.message = msg;
-        throw err;
-      }
-
-      const dnsResult = {
-        framework: "",
-        net: "",
-        netId: "",
-        addr: "",
-      };
-      dnsResult.framework = dataList[0];
-      for (let i = 1; i < dataList.length; i++) {
-        let list = dataList[i].split("=");
-        if (list[0] === "net") {
-          dnsResult.net = list[1];
-        } else if (list[0] === "netId") {
-          dnsResult.netId = list[1];
-        } else if (list[0] === "addr") {
-          dnsResult.addr = list[1];
+    if (data.Answer) {
+      for (const txt of data.Answer) {
+        if (url !== txt.name) {
+          const msg = "Mistached DNS name";
+          const err = new Error();
+          err.status = 400;
+          err.code = "BAD_REQUEST";
+          err.message = msg;
+          throw err;
         }
-      }
-      results.push(dnsResult);
-    }
+        let dataList = txt.data.replace(/['"]+/g, "").split(" ");
+        if (dataList[0] !== "openatts") {
+          continue;
+        }
 
+        const dnsResult = {
+          framework: "",
+          net: "",
+          netId: "",
+          addr: "",
+        };
+        dnsResult.framework = dataList[0];
+        for (let i = 1; i < dataList.length; i++) {
+          let list = dataList[i].split("=");
+          if (list[0] === "net") {
+            dnsResult.net = list[1];
+          } else if (list[0] === "netId") {
+            dnsResult.netId = list[1];
+          } else if (list[0] === "addr") {
+            dnsResult.addr = list[1];
+          }
+        }
+        results.push(dnsResult);
+      }
+    }
+    if (results.length === 0) {
+      const msg = "No DNS TXT record found";
+      const err = new Error();
+      err.status = 404;
+      err.code = "NOT_FOUND";
+      err.message = msg;
+      throw err;
+    }
     return results;
   } catch (err) {
-    err.status = 503;
-    err.code = "SERVICE_UNAVAILABLE";
-    err.message = "Failed to get DNS_TXT record";
     throw err;
   }
 };
